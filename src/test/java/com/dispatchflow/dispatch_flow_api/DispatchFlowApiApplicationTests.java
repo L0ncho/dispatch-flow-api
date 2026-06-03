ce9509a3a2b189c773083b2b023f5f33bbb06c34
Gustavo Dominguez <gus.dominguez@duocuc.cl>
2026-06-02 23:50:11 -0400

    chore: repo init

---
+++ src/test/java/com/dispatchflow/dispatch_flow_api/DispatchFlowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dispatchflow.dispatch_flow_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DispatchFlowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
