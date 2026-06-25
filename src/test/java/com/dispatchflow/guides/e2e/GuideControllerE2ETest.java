91b538e3bbafb98728089a5dc20d86b6a85e26a6
JR <jesusrivasvera22@gmail.com>
2026-06-24 20:35:16 -0400

    actualizando workflow 2

--- src/test/java/com/dispatchflow/guides/e2e/GuideControllerE2ETest.java
+++ src/test/java/com/dispatchflow/guides/e2e/GuideControllerE2ETest.java
@@ -4,8 +4,10 @@ import com.dispatchflow.dispatch_flow_api.DispatchFlowApiApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
@@ -33,6 +35,9 @@ class GuideControllerE2ETest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockitoBean
+    private JwtDecoder jwtDecoder;
 
     @DynamicPropertySource
     static void configureTestStorage(DynamicPropertyRegistry registry) throws Exception {
