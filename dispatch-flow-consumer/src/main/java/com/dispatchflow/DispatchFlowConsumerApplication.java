65c532413828251913d4c804b2ac9b06d10aac79
JR <jesusrivasvera22@gmail.com>
2026-07-09 23:27:20 -0400

    test: habilitar pipeline en rama de pruebas y ajustes finales de nube

--- dispatch-flow-consumer/src/main/java/com/dispatchflow/DispatchFlowConsumerApplication.java
+++ dispatch-flow-consumer/src/main/java/com/dispatchflow/DispatchFlowConsumerApplication.java
@@ -1,11 +1,11 @@
-package com.dispatchflow.consumer.dispatch_flow_consumer;
+package com.dispatchflow;
 
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @EnableRabbit
-@SpringBootApplication(scanBasePackages = "com.dispatchflow")
+@SpringBootApplication
 public class DispatchFlowConsumerApplication {
 
     public static void main(String[] args) {
